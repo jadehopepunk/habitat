ec47f2af668998896daaeb75cb1eea3781e54e4a
Craig Ambrose <craig@craigambrose.com>
2011-12-29 14:22:58 +1300

    Years input made smaller

--- lib/inputs/string_with_add_on_input.rb
+++ lib/inputs/string_with_add_on_input.rb
@@ -1,7 +1,7 @@
 class StringWithAddOnInput < SimpleForm::Inputs::Base
   
   def input
-    input_field = @builder.input_field(attribute_name)
+    input_field = @builder.input_field(attribute_name, options[:input_html])
     suffix_add_on = "<span class=\"add-on\"><span>#{options[:suffix]}</span>"
     
     "<div class=\"input-append\">#{input_field}#{suffix_add_on}</div>"
