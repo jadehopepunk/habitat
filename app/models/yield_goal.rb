f6334babb535e7dfe630de1af4e2d2de3545278b
Craig Ambrose <craig@craigambrose.com>
2011-12-29 14:17:52 +1300

    Displays most yield goals

--- app/models/yield_goal.rb
+++ app/models/yield_goal.rb
@@ -1,6 +1,7 @@
 class YieldGoal < ActiveRecord::Base
   COMMON_VOLUME_UNITS = %w(m^3 L gal)
   COMMON_WEIGHT_UNITS = %w(kg lb)
+  SPECIAL_USE_UNITS = %w(varieties)
   COMMON_TIME_UNITS = %w(week month year decade century)
   
   belongs_to :site
@@ -8,7 +9,7 @@ class YieldGoal < ActiveRecord::Base
   
   validates_presence_of :site, :use, :use_name
   validates_numericality_of :amount, :period, :lead_time_in_years, :allow_blank => true, :greater_than => 0
-  validates_inclusion_of :amount_unit, :in => COMMON_VOLUME_UNITS + COMMON_WEIGHT_UNITS
+  validates_inclusion_of :amount_unit, :in => COMMON_VOLUME_UNITS + COMMON_WEIGHT_UNITS + SPECIAL_USE_UNITS
   validates_inclusion_of :period_unit, :in => COMMON_TIME_UNITS
   
   def use_name
