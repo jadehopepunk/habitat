077968003fec9e1f9458d75dc382f48d62ceb0c3
Craig Ambrose <craig@craigambrose.com>
2012-01-08 20:38:43 +1300

    Added new feature form

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    goal_classes = [Goals::Yield, Goals::Feature, Goals::LabourInput, Goals::BudgetItem]
+    
     if user
       if user.role == 'admin'
         can :manage, :all
@@ -14,12 +16,12 @@ class Ability
         can :manage, Project, :user_id => user.id
         can :manage, Brief, {:project => {:user_id => user.id}}
 
-        can :manage, [Goals::Yield, Goals::LabourInput, Goals::BudgetItem], {:brief => {:project => {:user_id => user.id}}}
+        can :manage, goal_classes, {:brief => {:project => {:user_id => user.id}}}
       end
     else
       can :read, Project, :is_public => true
       can :read, Brief, {:project => {:is_public => true}}
-      can :read, [Goals::Yield, Goals::LabourInput, Goals::BudgetItem], {:brief => {:project => {:is_public => true}}}
+      can :read, goal_classes, {:brief => {:project => {:is_public => true}}}
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ SimpleFormBootstrap::Application.routes.draw do
       resources :yields do
         get :autocomplete_use_name, :on => :collection
       end
+      resources :features do
+      end
       resources :labour_inputs do
         get :autocomplete_job_name, :on => :collection
       end
