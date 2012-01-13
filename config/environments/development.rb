e278ada82d7e4537159483eb4fbdefa68f7a1b1e
Craig Ambrose <craig@craigambrose.com>
2012-01-13 15:21:20 +1300

    Added default gravatars

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ SimpleFormBootstrap::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.action_controller.asset_host = "habitat.dev"
 
   # Expands the lines which load the assets
   config.assets.debug = true
