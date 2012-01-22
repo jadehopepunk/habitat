1c002051a1564bd1b9baf1ff448edc774b064423
Craig Ambrose <craig@craigambrose.com>
2012-01-22 19:10:47 +1300

    Can edit first soil

--- features/projects/site/soils.feature
+++ features/projects/site/soils.feature
@@ -8,9 +8,7 @@ Feature: Project site has soils
       And I am on the "Site Analysis" project step
   
   Scenario: New project has one soil which you can edit
-  Then show me the page
     Given I am on the "Soil" site analysis step
-    Then show me the page
     Then I should see only 1 soil which is called "Site Soil"
     When I edit the soil
     And I submit valid soil data including the name "Sandy Loam"
--- features/step_definitions/soil_steps.rb
+++ features/step_definitions/soil_steps.rb
@@ -1,10 +1,16 @@
 Then /^I should see only (\d+) soil which is called "([^"]*)"$/ do |expected_number, expected_name|
-  page.should have_selector(".soils .soil", :count => expected_number)
-  page.should have_selector(".soils .soil .soil_name", :count => expected_number, :text => expected_name)
+  page.should have_selector("#soils .soil", :count => expected_number)
+  page.should have_selector("#soils .soil .soil_name", :count => expected_number, :text => expected_name)
 end
 
 When /^I edit the soil$/ do
   within('#soils_actions') do
     click_link 'Edit'
   end
+end
+
+When /^I submit valid soil data including the name "([^"]*)"$/ do |name|
+  fill_in 'Name', :with => name
+  fill_in 'Description', :with => "Lorem ipsum dolar sit amet"
+  click_button "Update Soil"
 end
